1f9574ebd8776b93d8be06b2f643cf146d9a93a0
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2020-08-17 19:20:28 -0600

    Updates misc.

--- src/Arichua/AluHammer.cs
+++ src/Arichua/AluHammer.cs
@@ -43,47 +43,8 @@ namespace TheXDS.Vulcanium.Arichua
 {
     public class AluHammer : Hammer
     {
-        private static readonly int[] knownPrimes = {
-            2,   3,   5,   7,   11,  13,  17,  19,  23,  29,
-            /*
-            31,  37,  41,  43,  47,  53,  59,  61,  67,  71, 
-            73,  79,  83,  89,  97,  101, 103, 107, 109, 113,
-            127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
-            179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
-            233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
-            283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
-            353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
-            419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
-            467, 479, 487, 491, 499, 503, 509, 521, 523, 541
-            */
-        };
 
-        protected bool IsPrime(int value)
-        {
-            if (value == 2) return true;
-            if (value == 1 || value % 2 == 0) return false;
-
-            foreach (var prime in knownPrimes)
-                if (value % prime == 0) return false;
-
-            var l = (int)Math.Sqrt(value);
-            for (int k = knownPrimes.Max() + 2; k <= l; k += 2)            
-                if (value % k == 0) return false;
-            
-
-            return true;
-        }
-
-        private readonly int[] array = InitArray();
-
-        private static int[] InitArray(int size = 1000000)
-        {
-            var c = new int[size];
-            var u = c.GetUpperBound(0);
-            var rnd = new Random();
-            for (var j = 0; j < u; j++) c[j] = rnd.Next(1, int.MaxValue);
-            return c;
-        }
+        private readonly int[] array = Magma.InitArray(1000000);
         
         public override Task Torture()
         {
@@ -100,7 +61,7 @@ namespace TheXDS.Vulcanium.Arichua
 
                 void TestIfPrime(int j)
                 {
-                    if (IsPrime(j)) lock(syncRoot) count++;
+                    if (Magma.IsPrime(j)) lock(syncRoot) count++;
                 }
                 try
                 {
---
+++ src/Tajumulco/Tajumulco.Magma/Magma.cs
@@ -0,0 +1,94 @@
+﻿/*
+MIT License
+
+Copyright (c) 2019-2020 César Andrés Morgan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+===============================================================================
+V U L C A N I U M - T A J U M U L C O
+  _.----._
+ (   (    )
+(  (    )  )
+ (________)
+    ||||
+  --++++--
+    ||||
+  .(    ).
+ (_(____)_)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TheXDS.Vulcanium.Tajumulco.Magma
+{
+    public static class Magma
+    {
+    }
+
+    /// <summary>
+    /// Abstrae el control y la transmisión de datos arbitrariamente grandes
+    /// por medio del protocolo TCP.
+    /// </summary>
+    public class TcpTunnel
+    {
+        public TcpClient Client { get; }
+        public ushort TxBufferSize { get; set; } = 32768;
+
+        public TcpTunnel(TcpClient client)
+        {
+            Client = client;
+            client.SendBufferSize
+        }
+
+        public async Task SendAsync(byte[] data)
+        {
+            Client.GetStream().Write(BitConverter.GetBytes(data.Length));
+            for (var j = 0; j < data.Length; j += TxBufferSize)
+            {
+                await Client.GetStream().WriteAsync(data.Skip(j).Take(TxBufferSize).ToArray());
+            }
+        }
+
+        public async Task<byte[]> ReceiveAsync()
+        {
+            var data = new List<byte>();
+            var btsz = new byte[4];
+            Client.GetStream().Read(btsz);
+            var tsz = BitConverter.ToInt32(btsz);
+            while (data.Count < tsz)
+            {
+                var bsz = 0;
+                while ((bsz = c.Available) > 0)
+                {
+                    var b = new byte [bsz];
+                    await c.GetStream().ReadAsync(b,0,bsz);
+                    data.AddRange(b);
+                }
+            }
+        }
+    }
+}
